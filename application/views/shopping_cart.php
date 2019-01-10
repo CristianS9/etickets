ece9d78c2864deae2b9459c39c454b35321c562d
Mikel Granero <mikelgmh5b09@gmail.com>
2019-01-10 12:42:11 +0100

    Haciendo el cálculo del carrito en local

--- application/views/shopping_cart.php
+++ application/views/shopping_cart.php
@@ -1,16 +1,15 @@
 <!DOCTYPE html>
-<html lang="en">
+<html ng-app="etickets" lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-            <?php echo script_tag("lib/jquery-3.3.1.min.js"); ?>
             <script src="<?php echo base_url() ?>lib/jquery-3.3.1.min.js"></script>
-            
-        <?php echo script_tag("js/shopping_cart.js"); ?>
+            <script src="<?php echo base_url() ?>lib/angular.min.js"></script>
+            <script src="<?php echo base_url() ?>js/shopping_cart.js"></script>
 </head>
-<body>
+<body ng-controller="eticketsController">
     <h1>Shopping cart</h1>
     <br>
     <br>
@@ -27,6 +26,8 @@
         echo("<input class='cantidad' id='$elemento->id' originalValue='$elemento->cantidad' type='number' name='' value='$elemento->cantidad'>");
         echo("<br>");
         echo ("Precio total: ".$elemento->precioTotal);
+        echo("<br>");
+        echo ("Precio angular: {{precio}}</span>");
         echo("<br>");
         echo ("Eliminar: <button class='deleteButton' id='$elemento->id'>Eliminar</button>");
         echo("<br>");
--- js/shopping_cart.js
+++ js/shopping_cart.js
@@ -1,10 +1,21 @@
+var etickets = angular.module("etickets", []);
+
+etickets.controller('eticketsController', function ($scope) {
+    $scope.precio = 'ferrefer';
+
+    $scope.SacarMensaje = function () {
+        $scope.mensaje = "Hola " + $scope.nombre + "" + $scope.apellido;
+    }
+});
+
 $(document).ready(function () {
     $(".cantidad").focusout(function () {
         var cantidad = $(this).val();
         var cantidadInicial = $(this).attr("originalValue");
         var id = $(this).attr('id');
         if (cantidad != cantidadInicial) {
-            changeQuantity(cantidad,id);
+            changeQuantity(cantidad, id);
+            recalcularPrecioPorCantidades(); //Terminar esto, tiene que recalcular los datos de ese artículo
         }
     });
 
@@ -13,7 +24,7 @@ $(document).ready(function () {
         deleteFromCart(id);
     });
 
-    function changeQuantity(pcantidad,pid) {
+    function changeQuantity(pcantidad, pid) {
         $.ajax({
             type: "POST",
             url: "ajax/EventDetail_Ajax/updateItemInCart",
@@ -29,20 +40,25 @@ $(document).ready(function () {
             }
         });
     }
-  function deleteFromCart(pentradaId) {
-      alert(pentradaId);
-      $.ajax({
-          type: "POST",
-          url: "ajax/EventDetail_Ajax/deleteFromCart",
-          data: {
-              entradaId: pentradaId
-          },
-          success: function (datos) {
-              alert("Borrado");
-          },
-          error: function (error) {
-              alert("No borrado");
-          }
-      });
-  }
+
+    function deleteFromCart(pentradaId) {
+        alert(pentradaId);
+        $.ajax({
+            type: "POST",
+            url: "ajax/EventDetail_Ajax/deleteFromCart",
+            data: {
+                entradaId: pentradaId
+            },
+            success: function (datos) {
+                alert("Borrado");
+            },
+            error: function (error) {
+                alert("No borrado");
+            }
+        });
+    }
+
+    function recalcularPrecioPorCantidades(){
+
+    }
 });
