dbcca54feb54a812b7d0b286ab59573505316bfe
Mikel Granero <mikelgmh5b09@gmail.com>
2019-01-23 11:34:27 +0100

    Cookie inútil pero está

--- application/views/elementos/footer.php
+++ application/views/elementos/footer.php
@@ -1,5 +1,13 @@
     <footer class="py-5 bg-dark">
         <div class="container">
-            <p class="m-0 text-center text-white ">Copyright &copy; Your Website 2018</p>
+            <p class="m-0 text-center text-white ">Última conexión: <?php
+            if (isset($this->session->id)){
+            echo $this->input->cookie('login');
+            }else{
+                echo "E-TICKETS";
+            }
+
+            ?></p>
+          
         </div>
     </footer>
