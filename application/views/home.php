122557d5eadbb97c836cdff4bec9199b998017a0
Mikel Granero <mikelgmh5b09@gmail.com>
2019-01-23 09:31:46 +0100

    header basiquísimo

--- application/views/home.php
+++ application/views/home.php
@@ -40,28 +40,8 @@ $handler->getJavascriptAntiBot();
 
 <body>
 
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-        <div class="container">
-            <a class="navbar-brand" href="#">E-TICKETS</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive"
-                aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarResponsive">
-                <ul class="navbar-nav ml-auto">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="#">Home <i class="fas fa-home"></i></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Carrito <i class="fas fa-shopping-cart"></i></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Perfil <i class="fas fa-user"></i></a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
+<?php $this->load->view('elementos/header'); ?>
+
 
     <div class="contenido">
         <h1 class="upcomming animated fadeIn">Próximos eventos <i id="lupa" class="pl-4 fas fa-search"> </i> <i><input
